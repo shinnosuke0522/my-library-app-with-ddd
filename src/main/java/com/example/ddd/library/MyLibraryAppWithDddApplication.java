c013d52d0cb1b1479e1c7b2833016222e1d413e9
Shinnosuke Hirota <shinnsouke.dev@gmail.com>
2024-04-04 01:24:39 +0900

    initial commit

---
+++ src/main/java/com/example/ddd/library/MyLibraryAppWithDddApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddd.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibraryAppWithDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLibraryAppWithDddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ddd/library/MyLibraryAppWithDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddd.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibraryAppWithDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
